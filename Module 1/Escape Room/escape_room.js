b2ec3e8a702e6ea4767eab9bcf54531cc981d923
Lawrence House <trehouse650@gmail.com>
2020-09-03 13:40:37 -0800

    Added while loop to escape room

--- Module 1/Escape Room/escape_room.js
+++ Module 1/Escape Room/escape_room.js
@@ -3,16 +3,28 @@ const readline = require("readline-sync")
 console.log(`Today was supposed to be a regular day, going to the store to pick up some Double Fudge Brownie ice cream and next thing you know you're seeing black. Now you see before you a room. The door is locked and we need to find the key. You see a hole in the wall, Do you . . . `)
 
 
-choices = ['Put hand in hole', 'Eind the key', 'Open the door']
+choices = ['Put hand in hole', 'Find the key', 'Open the door']
 
-index = readline.keyInSelect(choices, 'What do you do?')
-
-if (index == '0') {
-    console.log('You prick your finger on a needle and disintegrate!')
-} else if (index == '2') {
-    console.log(`The door is locked, didn't we check this already?`)
-} else if (index == '1') {
-    console.log(`You ignore the hole and search every inch of the room, finding a key!`)
-    console.log(`You test the key on the door and to your surprise, it opens! You walk outside steadily expecting the worst. Nothing happens. . . the END!`)
+key_found = false
+door_locked = true
+while (door_locked == true) {
+    index = readline.keyInSelect(choices, 'What do you do?')
+    if (index == '0') {
+        console.log('You prick your finger on a needle and disintegrate! The End!')
+        return
+    } else if (index == '2') {
+        if (key_found == true) {
+            console.log(`You try the key on the door and to your surprise, it opens! The End!`)
+            door_locked = false
+        } else if (key_found == false) {
+            console.log(`The door is locked, didn't we check this already?`)
+        }
+    } else if (index == '1') {
+        if (key_found == true) {
+            console.log(`You already searched the room!`)
+        } else if (key_found == false) {
+            console.log(`You ignore the hole and search every inch of the room, finding a key!`)
+            key_found = true
+        }
+    }
 }
-
