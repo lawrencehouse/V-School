5574306c9a46a34c2d569e5c782e98b00e428154
Lawrence House <trehouse650@gmail.com>
2020-09-03 16:50:14 -0800

    Finished Daily Planet Editor

--- Module 1/daily-planet-editor/paper.js
+++ Module 1/daily-planet-editor/paper.js
@@ -9,44 +9,42 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies = ["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var Enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-function Who_Wins(is-there-kryptonite, enemy_name) {
-if (!is-there-kryptonite) {
-    return "Superman beats " +enemyName+ ", of course";
+function WhoWins(is_there_kryptonite, EnemyName) {
+    if (is_there_kryptonite) {
+        return "Superman beats " + EnemyName + " of course";
+    } else {
+        return "Depends on how quick Superman can get rid of the Kryptonite. " + EnemyName + " could possibly win this one.";
+    }
 }
 
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
-}
-
-for (var i=0;i<enemies.length;i++)
-{
-    var is_there_kryptonite:
-    if (i%2=== 0) {
+for (var i = 0; i < Enemies.length; i++) {
+    var is_there_kryptonite = []
+    if (i%2 === 0) {
         is_there_kryptonite = true;
     } else {
         is_there_kryptonite = false;
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+    console.log(WhoWins(is_there_kryptonite, Enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe ()
-{
+function HowAttractedIsLoisLaneToMe() {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random() * 10) + 1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log(HowAttractedIsLoisLaneToMe());
 
-var clarkKent =true;
-var superman false;
+var clarkKent = true;
+var superman = false;
 
-while (clarkKent){
+while (clarkKent) {
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
-        superman=true;
-        console.log("Now I'm Superman!");}}
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+}
