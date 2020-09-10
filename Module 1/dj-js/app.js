737c186c34cee948edf6fca3873f238de697226c
Lawrence House <trehouse650@gmail.com>
2020-09-09 22:48:01 -0800

    Completed DJ-JS

--- Module 1/dj-js/app.js
+++ Module 1/dj-js/app.js
@@ -1,3 +1,53 @@
-document.getElementById('blue').addEventListener('mousedown', function() {
-    
+square = document.getElementById('dj')
+
+html = document.querySelector('html')
+
+square.addEventListener('mouseover', function() {
+    square.style.backgroundColor = 'blue'
+})
+
+square.addEventListener('mousedown', function() {
+    square.style.backgroundColor = 'red'
+})
+
+square.addEventListener('mouseup', function() {
+    square.style.backgroundColor = 'yellow'
+})
+
+square.addEventListener('dblclick', function() {
+    square.style.backgroundColor = 'green'
+})
+
+html.addEventListener('wheel', function() {
+    square.style.backgroundColor = 'orange'
+})
+
+html.addEventListener('keydown', function(event) {
+    if (event.keyCode == 66) {
+        square.style.backgroundColor = 'blue'
+    }
+})
+
+html.addEventListener('keydown', function(event) {
+    if (event.keyCode == 82) {
+        square.style.backgroundColor = 'red'
+    }
+})
+
+html.addEventListener('keydown', function(event) {
+    if (event.keyCode == 89) {
+        square.style.backgroundColor = 'yellow'
+    }
+})
+
+html.addEventListener('keydown', function(event) {
+    if (event.keyCode == 71) {
+        square.style.backgroundColor = 'green'
+    }
+})
+
+html.addEventListener('keydown', function(event) {
+    if (event.keyCode == 79) {
+        square.style.backgroundColor = 'orange'
+    }
 })
