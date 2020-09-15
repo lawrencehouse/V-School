c868e1bc3d9fb3cbb55b3dbb9980c27fe9157171
Lawrence House <trehouse650@gmail.com>
2020-09-14 23:35:54 -0800

    Added clear button EL

--- Module 1/dom-olympics-start/index.js
+++ Module 1/dom-olympics-start/index.js
@@ -1,7 +1,13 @@
+// Assigning selectors for 
+
+clear = document.querySelector('#clear-button')
+messages = document.querySelector('.messages')
 header1 = document.querySelector('#header')
 h1head = document.createElement('h1')
 h3head = document.createElement('h3')
 span1 = document.createElement('span')
+messageleft = document.querySelectorAll('.message left')
+messageright = document.querySelectorAll('.message right')
 
 header1.classList.add('header')
 h1head.innerHTML = '<b>JavaScript Made This!!</b>'
@@ -11,6 +17,12 @@ header1.appendChild(h1head)
 header1.appendChild(h3head)
 h3head.appendChild(span1)
 
-clear = document.querySelector('#clear-button')
-messages = document.querySelector('.messages')
 
+// Event listener to blank html within messages DIV
+
+clear.addEventListener('click', function() {
+    messages.innerHTML = ''
+})
+
+messageleft.innerText = 'Are you happy?'
+messageright.innerText = `Yes I'm happy!`
