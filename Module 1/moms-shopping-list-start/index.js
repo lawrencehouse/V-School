4806da9389b82f55274b2558ff909c6e4ced1fd6
Lawrence House <trehouse650@gmail.com>
2020-09-22 23:15:50 -0800

    testing part 2

--- Module 1/moms-shopping-list-start/index.js
+++ Module 1/moms-shopping-list-start/index.js
@@ -1,6 +1,10 @@
 const form = document['addItem']
 const list = document.querySelector('#list')
 const item = document.querySelector('#title')
+itemlist = list.children.length
+erase = document.getElementById('erase')
+
+
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -9,20 +13,19 @@ form.addEventListener('submit', e => {
     const xbtn = document.createElement('button')
     new_title.innerText = form.title.value
     editbtn.innerText = 'edit'
+    xbtn.id = 'erase'
     xbtn.innerText = 'X'
+
+    xbtn.addEventListener('click', () => {
+        for (i=0; i < itemlist; i++) {
+            list.removeChild(list.childNodes[i])
+        }
+    })
+    
     const new_li = document.createElement('li')
     list.append(new_li)
     list.lastChild.append(new_title)
     list.lastChild.append(editbtn)
     list.lastChild.append(' ')
     list.lastChild.append(xbtn)
-})
-
-itemlist = list.children.length
-
-xbtn.addEventListener('click', () => {
-    for (i=0; i < itemlist; i++) {
-        
-    }
-
 })
