f3cb9ce5491d2f714b8c3b4183ba75ab63bd1b05
Lawrence House <trehouse650@gmail.com>
2020-09-22 22:44:02 -0800

    Shopping list part 1 complete

--- Module 1/moms-shopping-list-start/index.js
+++ Module 1/moms-shopping-list-start/index.js
@@ -1,8 +1,28 @@
-form = document.querySelector('#add-todo')
-list = document.querySelector('#list')
-item = document.querySelector('#title')
-new_li = document.createElement('li')
+const form = document['addItem']
+const list = document.querySelector('#list')
+const item = document.querySelector('#title')
 
-form.addEventListener('click', () => {
-    list.appendChild(new_li)
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const new_title = document.createElement('div')
+    const editbtn = document.createElement('button')
+    const xbtn = document.createElement('button')
+    new_title.innerText = form.title.value
+    editbtn.innerText = 'edit'
+    xbtn.innerText = 'X'
+    const new_li = document.createElement('li')
+    list.append(new_li)
+    list.lastChild.append(new_title)
+    list.lastChild.append(editbtn)
+    list.lastChild.append(' ')
+    list.lastChild.append(xbtn)
+})
+
+itemlist = list.children.length
+
+xbtn.addEventListener('click', () => {
+    for (i=0; i < itemlist; i++) {
+        
+    }
+
 })
