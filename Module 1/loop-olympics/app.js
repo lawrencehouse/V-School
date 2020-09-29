93e60ea678640b5519099cbc3efe6500bbe070a2
Lawrence House <trehouse650@gmail.com>
2020-09-28 22:38:06 -0800

    Loop olympics 1 and 2 done

---
+++ Module 1/loop-olympics/app.js
@@ -0,0 +1,54 @@
+// Preliminaries
+
+// 1
+
+// for (i = 0; i <= 9; i++) {
+//     console.log(i)
+// }
+
+// 2
+
+// for (i = 9; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// 3
+
+// var fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for (i = 0; i < fruit.length; i++) {
+//     console.log(fruit[i])
+// }
+
+
+// Bronze Medal
+
+// 1
+
+// numbers09 = []
+
+// for (i = 0; i <= 9; i++) {
+//     numbers09.push(i)
+// }
+
+// console.log(numbers09)
+
+// 2
+
+// for (i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// console.log(numbers100)
+
+// 3
+
+var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+newfruit = []
+
+for (i = 0; i < fruit.length; i++) {
+    
+}
