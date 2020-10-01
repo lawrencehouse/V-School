203dfe3479ca1bc93d2be81a9588b550b302faba
Lawrence House <trehouse650@gmail.com>
2020-09-30 23:28:52 -0800

    Completed Array methods

--- Module 1/array-methods/app.js
+++ Module 1/array-methods/app.js
@@ -2,3 +2,15 @@ var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 
 vegetables.pop('lettuce')
+fruit.shift('banana')
+fruit.push(fruit.indexOf('orange'))
+vegetables.push(vegetables.length)
+food = fruit.concat(vegetables)
+food.splice(4, 2)
+food.reverse()
+
+
+// console.log(vegetables)
+// console.log(fruit)
+
+console.log(food.toString())
