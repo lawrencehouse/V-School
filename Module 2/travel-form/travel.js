d107a64a9444da6ae5a2dbafe53255bda14cf32b
Lawrence House <trehouse650@gmail.com>
2020-11-12 23:57:09 -0900

    Completed functionality of travel form

--- Module 2/travel-form/travel.js
+++ Module 2/travel-form/travel.js
@@ -2,5 +2,15 @@ form = document['traveler-info']
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    
+    first_name = form.first_name.value
+    last_name = form.last_name.value
+    age = form.age.value
+    gender = form.gender.value
+    country = form.location.value
+    checkedDiet = []
+    checkedBoxes = document.querySelectorAll('input[name=diet]:checked')
+    for (i = 0; i < checkedBoxes.length; i++) {
+        checkedDiet.push(checkedBoxes[i].value)
+    }
+    alert(`First name: ${first_name}\nLast name: ${last_name}\nAge: ${age}\nGender: ${gender}\nLocation: ${country}\nDietary restrictions: ${checkedDiet}`)
 })
