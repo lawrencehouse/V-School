9031b3800ddb9f90903c4dfdd9e3f4ff2fb490a3
Lawrence House <trehouse650@gmail.com>
2020-11-13 22:40:13 -0900

    Starting mario pest

---
+++ Module 2/mario-pest-control/invoice.js
@@ -0,0 +1,14 @@
+baddies = [
+    {
+        name: 'Goomba',
+        price: 5,
+    },
+    {
+        name: 'Bob-ombs',
+        price: 7,
+    },
+    {
+        name: 'Cheep-cheeps',
+        price: 11,
+    }
+]
