e9ead5ca8c29e649b3b2fdb70f7da9be1564e48b
Lawrence House <trehouse650@gmail.com>
2020-11-16 22:09:14 -0900

    Finished backend for pest control

--- Module 2/mario-pest-control/invoice.js
+++ Module 2/mario-pest-control/invoice.js
@@ -1,14 +1,19 @@
-baddies = [
-    {
-        name: 'Goomba',
-        price: 5,
-    },
-    {
-        name: 'Bob-ombs',
-        price: 7,
-    },
-    {
-        name: 'Cheep-cheeps',
-        price: 11,
-    }
-]
+total = document.getElementById('total')
+
+form = document['baddies']
+
+goomba_caught = document.getElementById('goom')
+
+bobs_caught = document.getElementById('bob')
+
+cheeps_caught = document.getElementById('cheep')
+
+total.innerText = 0 + ' Coins'
+
+form.addEventListener('change', e => {
+    goomba_total = Math.floor(goomba_caught.value * 5)
+    bob_total = Math.floor(bobs_caught.value * 7)
+    cheep_total = Math.floor(cheeps_caught.value * 11)
+    baddie_total = goomba_total + bob_total + cheep_total
+    total.innerText = baddie_total + ' Coins'
+})
