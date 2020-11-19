80539d48f6a5b3e9c7ba017821a309e79ff5c7aa
Lawrence House <trehouse650@gmail.com>
2020-11-18 23:39:42 -0900

    test 2 done

--- Module 2/array-map-exercises/array-map.js
+++ Module 2/array-map-exercises/array-map.js
@@ -9,3 +9,26 @@ function doubleNumbers(arr){
   }
   
 console.log(doubleNumbers([2, 5, 100])) // [4, 10, 200]
+
+// 2. Take an array of numbers and make them strings
+
+function stringItUp(arr){
+  arr2 = []
+  arr.map(function(number) {
+    arr2.push(number.toString())
+  })
+  return arr2
+}
+
+console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+
+// 3. Capitalize each of an array of names/
+
+function capitalizeNames(arr){
+  arr2 = []
+  arr.map(function(name) {
+    arr2.push()
+  })
+}
+
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
