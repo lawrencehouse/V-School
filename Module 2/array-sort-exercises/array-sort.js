541c06668b1bc6d0da50258ce42461fac38309ce
Lawrence House <trehouse650@gmail.com>
2020-11-20 12:42:13 -0900

    Finished sort exercises

--- Module 2/array-sort-exercises/array-sort.js
+++ Module 2/array-sort-exercises/array-sort.js
@@ -15,3 +15,39 @@ function greatestToLeast(arr) {
   }
   
   console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1] 
+
+// 3. Sort an array from shortest string to longest
+
+function lengthSort(arr) {
+  result = arr.sort((a,b) => a.length - b.length)
+  return result
+}
+
+console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+
+// 4. Sort an array alphabetically
+
+function alphabetical(arr) {
+  result = arr.sort()
+  return result
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"] 
+
+// 5. Sort the objects in the array by age
+
+function byAge(arr){
+  result = arr.sort((a, b) => a.age - b.age)
+  return result
+}
+
+console.log(byAge([
+    { name: "Quiet Samurai", age: 22 },
+    { name: "Arrogant Ambassador", age: 100 },
+    { name: "Misunderstood Observer", age: 2 },
+    { name: "Unlucky Swami", age: 77 }
+]));
+// => [ { name: 'Misunderstood Observer', age: 2 },
+//  { name: 'Quiet Samurai', age: 22 },
+//  { name: 'Unlucky Swami', age: 77 },
+//  { name: 'Arrogant Ambassador', age: 100 } ]
