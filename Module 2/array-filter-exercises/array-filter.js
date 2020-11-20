59c7601daaa026e43462f71979c9388b82322689
Lawrence House <trehouse650@gmail.com>
2020-11-19 23:53:49 -0900

    Finished test 1 on filters

--- Module 2/array-filter-exercises/array-filter.js
+++ Module 2/array-filter-exercises/array-filter.js
@@ -1,11 +1,8 @@
 // 1. Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 
 function fiveAndGreaterOnly(arr) {
-    arr2 = []
-    arr.filter(function(number) {
-        result = number >= 5
-        return result
-    })
+    result = arr.filter(number => number >= 5)
+    return result
   }
 
   // test
