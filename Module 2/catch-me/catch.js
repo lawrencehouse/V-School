63099a03a02e13e7ccd82697bfe811fbf91d2230
Lawrence House <trehouse650@gmail.com>
2020-12-06 23:30:43 -0900

    starting catch me

--- Module 2/catch-me/catch.js
+++ Module 2/catch-me/catch.js
@@ -1,9 +1,25 @@
-function sum(x, y){
-    if ((typeof x != 'number') || (typeof y != 'number')) {
-        throw 'Both inputs must be numbers'
-    } else {
-        return x + y;
-    } 
-  }
+dog = 'dog'
 
-console.log(sum(1,2))
+// 1a
+
+function sum(x, y) {
+    try {
+        if ((typeof x != 'number') || (typeof y != 'number')) {
+            throw 'Both inputs must be numbers'
+        } else {
+            console.log('Your code works fine!')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    return x + y
+}
+
+// 1b
+
+try {
+    sum('1','2')
+} catch(err) {
+    console.log('test')
+}
+
