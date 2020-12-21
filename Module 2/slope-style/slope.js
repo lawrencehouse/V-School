80f669a4de99d62f03861cab3b11da1b7735b0f1
Lawrence House <trehouse650@gmail.com>
2020-12-20 23:02:01 -0900

    starting slope

---
+++ Module 2/slope-style/slope.js
@@ -0,0 +1,8 @@
+
+
+function collectAnimals(/*your code here*/) {  
+    /*and here*/ 
+}
+
+collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"); 
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
