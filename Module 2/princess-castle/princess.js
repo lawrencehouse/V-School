71a7514ef552511bf1a335294ae9af9e4cefb620
Lawrence House <trehouse650@gmail.com>
2020-12-31 15:26:27 -0800

    Finished princess-in-another-castle

--- Module 2/princess-castle/princess.js
+++ Module 2/princess-castle/princess.js
@@ -1,10 +1,10 @@
 class Player {
-    constructor(name, totalCoins, status, hasStar, gameActive) {
+    constructor(name, status) {
         this.name = name
-        this.totalCoins = totalCoins
+        this.totalCoins = 0
         this.status = status
-        this.hasStar = hasStar
-        this.gameActive = gameActive
+        this.hasStar = false
+        this.gameActive = true
     }
 
     setName(namePicked) {
@@ -12,20 +12,28 @@ class Player {
     }
 
     gotHit() {
-        if (status == 'Powered Up') {
-            status = 'Big'
-        } else if (status == 'Big') {
-            status = 'Small'
-        } else if (status == 'Small') {
-            status = 'Dead'
-        } else {
-            console.log('You are dead')
+        if (this.hasStar == true) {
+            console.log('Your star protected you!')
+            this.hasStar = false
+        } else if (this.status == 'Powered Up') {
+            this.status = 'Big'
+        } else if (this.status == 'Big') {
+            this.status = 'Small'
+        } else if (this.status == 'Small') {
+            this.status = 'Dead'
             this.gameActive = false
         }
     }
     
     gotPowerup() {
-        if ()
+        if (this.status == 'Small') {
+            this.status = 'Big'
+        } else if (this.status == 'Big') {
+            this.status = 'Powered Up'
+        } else if (this.status == 'Powered Up') {
+            console.log('Congrats! You got a star!')
+            this.hasStar = true
+        }
     }
 
     addCoin() {
@@ -33,33 +41,35 @@ class Player {
     }
 
     print() {
-        console.log(`Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.totalCoins}`)
         if (this.hasStar == true) {
             console.log('You have a star!')
         }
+        console.log(`Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.totalCoins}`)
+        console.log('\n')
     }
 }
 
-const Dunkman = new Player('Mario', 0, 'small', false, true)
+const Dunkman = new Player('Mario', 'Small')
 
 getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    randomInt = Math.floor(Math.random() * (max - min + 1)) + min
     if (randomInt == 0) {
-        Dunkman.gotHit
+        console.log('*WHAM*\n')
+        Dunkman.gotHit()
     } else if (randomInt == 1) {
-        Dunkman.gotPowerup
+        Dunkman.gotPowerup()
     } else {
-        Dunkman.addCoin
+        Dunkman.addCoin()
     }
+    Dunkman.print()
 }
 
-gameplay = setInterval(console.log(getRandomInt(0,2)), 2000)
-
-stopFunction = () => {
-    if (gameActive === false) {
+gameplay = setInterval(() => {
+    getRandomInt(0,2)
+    if (Dunkman.gameActive === false) {
         console.log('You have died!')
-        clearInterval(gameReplay)
+        clearInterval(gameplay)
     }
-}
+}, 1000)
