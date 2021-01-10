0157098b1275c2b6234bf47f6f7bb64c35885816
Lawrence House <trehouse650@gmail.com>
2021-01-09 19:49:48 -0900

    Completed pokemon list

--- Module 2/pokemon-list/pokemon.js
+++ Module 2/pokemon-list/pokemon.js
@@ -1,19 +1,21 @@
 const xhr = new XMLHttpRequest();
 
+
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
+        // 7-9 Gets the data
         const jsonData = xhr.responseText;
         const data = JSON.parse(jsonData);
-        // document.getElementById("demo1").textContent = "JSON results: " + jsonData;
-        document.getElementById("demo2").textContent = "Parsed JSON's 'name' property: " + data.name;
         const pokemon = data['objects'][0]['pokemon'];
-        console.log(pokemon);
+        // 11-15 Maps the data by pokemon name
+        pokemon.map(name => {
+            list = document.createElement('li');
+            order = document.getElementById('pokelist');
+            list.innerHTML = `${name.name}`;
+            order.appendChild(list);
+        })
     }
 };
 
 xhr.open("GET", "https://api.vschool.io/pokemon", true);
 xhr.send();
-
-allPokemon = (pokemon) => pokemon.map(pokename => document.getElementById('pokename').textContent = pokename)
-
-allPokemon(pokemon)
