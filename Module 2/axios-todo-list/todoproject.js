132f92e49fde5f5ed3bc3007a67a41c163f2202e
Lawrence House <trehouse650@gmail.com>
2021-01-14 22:17:09 -0900

    axios get finished

--- Module 2/axios-todo-list/todoproject.js
+++ Module 2/axios-todo-list/todoproject.js
@@ -1,10 +1,20 @@
 // const axios = require('axios')
 
-axios.get('https://api.vschool.io/scrimbalessons/todo')
+// axios.post('https://api.vschool.io/lawrencehouse/todo', )
+
+axios.get('https://api.vschool.io/lawrencehouse/todo')
     .then(res => {
+        console.log(res)
         for (i=0; res.data.length; i++) {
             const h2 = document.createElement('h2')
-            h2.textContent = res.data[i].title
+            if (res.data[i].completed) {
+                h2.innerHTML = `<s>${res.data[i].title}</s>`
+            } else {
+                h2.textContent = res.data[i].title
+            }
+            if (res.data[i].imgUrl != undefined) {
+                h2.innerHTML += `<img src="${res.data[i].imgUrl}" width="100" height="100">`
+            }
             document.body.appendChild(h2)
         }
     })
