bd1bc96833806c5bb945ef6a3e184b0c63438066
Lawrence House <trehouse650@gmail.com>
2021-01-16 23:47:07 -0900

    post finished

--- Module 2/axios-todo-list/todoproject.js
+++ Module 2/axios-todo-list/todoproject.js
@@ -8,16 +8,16 @@ axios.get('https://api.vschool.io/lawrencehouse/todo')
         console.log(res)
         todolist = document.getElementById('todolist')
         for (i=0; res.data.length; i++) {
-            const h2 = document.createElement('h2')
+            const li = document.createElement('li')
             if (res.data[i].completed) {
-                h2.innerHTML = `<s>${res.data[i].title}</s>`
+                li.innerHTML = `<s>${res.data[i].title}</s>`
             } else {
-                h2.textContent = res.data[i].title
+                li.textContent = res.data[i].title
             }
             if (res.data[i].imgUrl != undefined) {
-                h2.innerHTML += `<img src="${res.data[i].imgUrl}" width="100" height="100">`
+                li.innerHTML += `<img src="${res.data[i].imgUrl}" width="100" height="100">`
             }
-            document.body.appendChild(h2)
+            todolist.appendChild(li)
         }
     })
     // .then(res => res.map(todo => {
@@ -28,17 +28,14 @@ axios.get('https://api.vschool.io/lawrencehouse/todo')
     .catch(err => console.log(err))
 
 todoForm.addEventListener("submit", function(event){
-    event.preventDefault()
-    
+    event.preventDefault()  
     const newTodo = {
         title: todoForm.title.value,
         price: todoForm.price.value,
         description: todoForm.description.value,
         imgUrl: todoForm.imgUrl.value
-    }
-    
+    }   
     axios.post("https://api.vschool.io/lawrencehouse/todo", newTodo)
         .then(response => console.log(response.data))
-        .catch(error => console.log(error))
-    
+        .catch(error => console.log(error))  
 })
