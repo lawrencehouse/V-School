4c44e921de4c3c2e214969593ca68b45ba6d0579
Lawrence House <trehouse650@gmail.com>
2021-01-17 22:59:36 -0900

    Images fixed

--- Module 2/axios-todo-list/todoproject.js
+++ Module 2/axios-todo-list/todoproject.js
@@ -1,5 +1,7 @@
 // const axios = require('axios')
 
+// const { default: axios } = require("axios")
+
 // axios.post('https://api.vschool.io/lawrencehouse/todo', )
 
 axios.get('https://api.vschool.io/lawrencehouse/todo')
@@ -8,13 +10,17 @@ axios.get('https://api.vschool.io/lawrencehouse/todo')
         console.log(res)
         todolist = document.getElementById('todolist')
         for (i=0; res.data.length; i++) {
+            var checkbox = document.createElement("INPUT")
+            checkbox.type = 'checkbox'
+            checkbox.name = 'completed_box'
             const li = document.createElement('li')
             if (res.data[i].completed) {
                 li.innerHTML = `<s>${res.data[i].title}</s>`
             } else {
                 li.textContent = res.data[i].title
             }
-            if (res.data[i].imgUrl != undefined) {
+            
+            if (res.data[i].imgUrl != "") {
                 li.innerHTML += `<img src="${res.data[i].imgUrl}" width="100" height="100">`
             }
             todolist.appendChild(li)
@@ -39,3 +45,7 @@ todoForm.addEventListener("submit", function(event){
         .then(response => console.log(response.data))
         .catch(error => console.log(error))  
 })
+
+// axios.put('https://api.vschool.io/lawrencehouse/todo/<id>', {})
+//     .then(res => console.log(res.data))
+//     .catch(err => console.log(err))
