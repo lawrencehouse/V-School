57bf7998f0d54bd7318dbd8931e08a965c7fecba
Lawrence House <trehouse650@gmail.com>
2021-01-19 00:44:49 -0900

    tired

--- Module 2/axios-todo-list/todoproject.js
+++ Module 2/axios-todo-list/todoproject.js
@@ -2,50 +2,84 @@
 
 // const { default: axios } = require("axios")
 
-// axios.post('https://api.vschool.io/lawrencehouse/todo', )
-
-axios.get('https://api.vschool.io/lawrencehouse/todo')
-
-    .then(res => {
-        console.log(res)
-        todolist = document.getElementById('todolist')
-        for (i=0; res.data.length; i++) {
-            var checkbox = document.createElement("INPUT")
-            checkbox.type = 'checkbox'
-            checkbox.name = 'completed_box'
-            const li = document.createElement('li')
-            if (res.data[i].completed) {
-                li.innerHTML = `<s>${res.data[i].title}</s>`
-            } else {
-                li.textContent = res.data[i].title
-            }
-            
-            if (res.data[i].imgUrl != "") {
-                li.innerHTML += `<img src="${res.data[i].imgUrl}" width="100" height="100">`
-            }
-            todolist.appendChild(li)
-        }
-    })
-    // .then(res => res.map(todo => {
-    //     const h2 = document.createElement('h2')
-    //     h2.textContent = todo.data.title
-    //     document.body.appendChild(h2)
-    // }))
+grabData = () => {
+    axios.get('https://api.vschool.io/lawrencehouse/todo')
+    .then(res => listOfTodos(res.data))
     .catch(err => console.log(err))
+}
 
-todoForm.addEventListener("submit", function(event){
-    event.preventDefault()  
+listOfTodos = data => {
+    todolist = document.getElementById('todolist')
+
+    for (i=0; data.length; i++) {
+        const checkbox = document.createElement("INPUT")
+        checkbox.type = 'checkbox'
+        checkbox.id = `completed_box${i}`
+        const li = document.createElement('li')
+        li.innerHTML += ``
+        const deleteBtn= document.createElement('button') 
+        deleteBtn.textContent="DELETE";
+        deleteBtn.value = 'delete';
+        
+        if (data[i].completed) {
+            li.innerHTML = `<s>${data[i].title}</s> `
+        } else {
+            li.textContent = data[i].title
+        }
+        
+        todolist.appendChild(li)
+        li.appendChild(checkbox)
+        li.appendChild(deleteBtn)
+
+        completed_box = document.getElementById(`completed_box${i}`)
+        id = data[i]._id
+
+        completed_box.addEventListener('change', function() {
+            if (completed_box.checked) {
+                updates = {
+                    completed: true
+                }
+            } else {
+                updates = {
+                    completed: false
+                }
+            }
+
+
+            
+            axios.put(`https://api.vschool.io/lawrencehouse/todo/${id}`, updates)
+                .then(res => res.data)
+                .catch(err => console.log(err))
+        })   
+
+        deleteBtn.addEventListener('click', function(){
+            console.log('delete button was clicked');
+            id = data[i]._id;
+            url = `https://api.vschool.io/lawrencehouse/todo/${id}`
+            axios.delete(url)
+              .then(response => console.log(response.data))
+              .then(error => console.log(error));
+    
+            location.reload();
+        })
+    }
+}
+
+// Submit new todo to API
+
+todoForm.addEventListener("submit", function(e){
+    e.preventDefault()  
+
     const newTodo = {
         title: todoForm.title.value,
         price: todoForm.price.value,
         description: todoForm.description.value,
         imgUrl: todoForm.imgUrl.value
     }   
+
     axios.post("https://api.vschool.io/lawrencehouse/todo", newTodo)
-        .then(response => console.log(response.data))
+        .then(response => grabData())
         .catch(error => console.log(error))  
 })
 
-// axios.put('https://api.vschool.io/lawrencehouse/todo/<id>', {})
-//     .then(res => console.log(res.data))
-//     .catch(err => console.log(err))
+grabData()
