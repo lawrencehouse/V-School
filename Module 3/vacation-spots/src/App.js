1fa430646f9ebdc3cbd386d48e445b32394d8c57
Lawrence House <trehouse650@gmail.com>
2021-02-05 17:20:28 -0900

    Finished vacation spots (need to style)

--- Module 3/vacation-spots/src/App.js
+++ Module 3/vacation-spots/src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
 import './App.css';
+import vacationSpots from "./vacationspots"
+import Card from "./vacation"
 
 function App() {
+    const Vacation = vacationSpots.map(spot => <Card place={spot.place} goTime={spot.timeToGo} price={spot.price} />)
     return (
         <div className="App">
-            <h1>Hello</h1>
+            {Vacation}
         </div>
     );
 }
--- Module 3/vacation-spots/src/vacation.js
+++ Module 3/vacation-spots/src/vacation.js
@@ -1,4 +1,20 @@
 import React from "react"
-import vacationSpots from "./vacationspots"
 
-const Vacay = (vacationSpots) => vacationSpots.map(spot => <Card Place={} />)
+const Card = (props) => {
+    // if (props.price < 500) {
+    //     "$"
+    // } else if (props.price < 1000) {
+    //     "$$"
+    // } else {
+    //     "$$$"
+    // }
+    return (
+        <div className="vacay-card">
+            <h1>Escape away to <b><i>{props.place}</i></b>!</h1>
+            <h2>Cost: {props.price}</h2>
+            <h2>The best time to go is in {props.goTime}</h2>
+        </div>
+    )
+}
+
+export default Card
