37baf1a06b830a21c5e8defa35f9ec517f82c144
Lawrence House <trehouse650@gmail.com>
2021-02-18 00:04:00 -0900

    trying

--- Module 3/dj-react/src/App.js
+++ Module 3/dj-react/src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
+import Square from './squares'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className='square'></h1>
-      <h1 className='square'></h1>
-      <h1 className='square'></h1>
-      <h1 className='square'></h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1 className='square'></h1>
+//       <h1 className='square'></h1>
+//       <h1 className='square'></h1>
+//       <h1 className='square'></h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            colors: ['white', 'white', 'white', 'white']
+        }
+    }
+
+    render() {
+        const squares = this.state.colors.map(color => <Square color={this.state.color}/>)
+        
+
+        return (
+            <div>
+                {squares}
+            </div>
+        )
+    }
 }
 
 export default App;
--- Module 3/dj-react/src/squares.js
+++ Module 3/dj-react/src/squares.js
@@ -1,18 +1,11 @@
 import Component from 'react'
 
-class Square extends Component {
-    constructor() {
-        super()
-        this.state = {
-            colors: ['white', 'white', 'white', 'white']
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>{this.state.colors}</h1>
-            </div>
-        )
-    }
+function Square(props) {
+    return (
+        <div>
+            
+        </div>
+    )
 }
+
+export default Square
