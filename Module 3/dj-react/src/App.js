9c832b22f79cc37f30b6318b1351dcbfd746f1df
Lawrence House <trehouse650@gmail.com>
2021-02-25 21:24:51 -0900

    Completed DJ react

--- Module 3/dj-react/src/App.js
+++ Module 3/dj-react/src/App.js
@@ -20,12 +20,45 @@ class App extends Component {
             colors: ['white', 'black', 'blue', 'white']
         }
         this.djSmall = this.djSmall.bind(this)
+        this.partyDJ = this.partyDJ.bind(this)
+        this.leftBlue = this.leftBlue.bind(this)
+        this.rightBlue = this.rightBlue.bind(this)
     }
 
     djSmall() {
         this.setState(prevState => {
+            if (prevState.colors[0] == 'white') {
+                return {
+                    colors: ['black', 'black', 'black', 'black']
+                }
+            } else {
+                return {
+                    colors: ['white', 'white', 'white', 'white']
+                }
+            }
+        })
+    }
+
+    partyDJ() {
+        this.setState(prevState => {
             return {
-                colors: prevState.count + 1
+                colors: ['purple', 'purple', prevState.colors[2], prevState.colors[3]]
+            }
+        })
+    }
+
+    leftBlue() {
+        this.setState(prevState => {
+            return {
+                colors: [prevState.colors[0], prevState.colors[1], 'blue', prevState.colors[3]]
+            }
+        })
+    }
+
+    rightBlue() {
+        this.setState(prevState => {
+            return {
+                colors: [prevState.colors[0], prevState.colors[1], prevState.colors[2], 'blue']
             }
         })
     }
@@ -36,10 +69,10 @@ class App extends Component {
         return (   
             <div className="App">
                 {squares}
-                <button>DJ Small</button>
-                <button>Party DJ</button>
-                <button>Left Blue</button>
-                <button>Right Blue</button>
+                <button onClick={this.djSmall}>DJ Small</button>
+                <button onClick={this.partyDJ}>Party DJ</button>
+                <button onClick={this.leftBlue}>Left Blue</button>
+                <button onClick={this.rightBlue}>Right Blue</button>
             </div>
         )
     }
