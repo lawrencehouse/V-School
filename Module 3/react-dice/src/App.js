54ed5d98a69930d898738fa8c77553cd423f4197
Lawrence House <trehouse650@gmail.com>
2021-02-25 23:21:44 -0900

    started class App;started diceRoll setstate

--- Module 3/react-dice/src/App.js
+++ Module 3/react-dice/src/App.js
@@ -1,11 +1,34 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            num1: 0,
+            num2: 0,
+            num3: 0,
+            num4: 0,
+            num5: 0
+        }
+        this.diceRoll = this.diceRoll.bind(this)
+    }
+
+    diceRoll() {
+        this.setState(prevState => {
+            
+        })
+    }
+
+    render() {
+        
+
+        return (
+            <div className="App">
+                <button onClick={this.diceRoll}>Roll Dice</button>
+            </div>
+          )
+    }
 }
 
 export default App;
